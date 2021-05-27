06c66e11a21bad5de56d32dd312a5d66fdb8590f
megan black <blackme@oregonstate.edu>
2021-05-27 08:28:13 -0700

    Implemented / 4 but not / 100 method within leap function in leapyear.py. This code allows for our test written to pass

---
+++ leapyear.py
@@ -0,0 +1,5 @@
+
+def leap(year):
+  year = int(year)
+  if year % 4 == 0 and year % 100 != 0:
+    return str(year) + " is a leap year"
--- test_leapyear.py
+++ test_leapyear.py
@@ -3,7 +3,7 @@ import leapyear
 
 class TestCase(unittest.TestCase):
   def test_good1(self):
-    self.assertEqual(leap(2020), "2020 is a leap year")
+    self.assertEqual(leapyear.leap(2020), "2020 is a leap year")
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
