fe14a07ca0b2e86addd0dffa3eb9b8433c38cf75
megan black <blackme@oregonstate.edu>
2021-05-27 08:29:25 -0700

    Wrote second test, testing if year divisible by 4, 100, and 400 is a leap year

--- test_leapyear.py
+++ test_leapyear.py
@@ -4,6 +4,8 @@ import leapyear
 class TestCase(unittest.TestCase):
   def test_good1(self):
     self.assertEqual(leapyear.leap(2020), "2020 is a leap year")
+def test_good2(self):
+self.assertEqual(leapyear.leap(2000), "2000 is a leap year")
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
