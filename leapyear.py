7dfe89a50d7434c8ded9bae8a66b639bbfd13a00
megan black <blackme@oregonstate.edu>
2021-05-27 08:34:24 -0700

    Implemented else method, for all years that are not leap years

--- leapyear.py
+++ leapyear.py
@@ -3,5 +3,7 @@ def leap(year):
     year = int(year)
     if year % 4 == 0 and year % 100 != 0:
         return str(year) + " is a leap year"
-    if year % 4 == 0 and year % 100 == 0 and year % 400 == 0:
+    elif year % 4 == 0 and year % 100 == 0 and year % 400 == 0:
         return str(year) + " is a leap year"
+    else: 
+        return str(year) + " is not a leap year"
