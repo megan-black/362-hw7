a7c2f9e1241175e00ac98dabd416ae94e26f702c
megan black <blackme@oregonstate.edu>
2021-05-27 08:26:12 -0700

    Wrote first test, testing if year divisible by 4 but not 100 is a leap year

---
+++ test_leapyear.py
@@ -0,0 +1,9 @@
+import unittest
+import leapyear
+
+class TestCase(unittest.TestCase):
+  def test_good1(self):
+    self.assertEqual(leap(2020), "2020 is a leap year")
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
