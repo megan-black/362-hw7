69ff71fbade07b2b965c5329236d299d8bdd3c09
megan black <blackme@oregonstate.edu>
2021-05-27 08:31:33 -0700

    Implemented method tested in test_good2, where year divisible by 4 100 and 400 is a leap year

--- leapyear.py
+++ leapyear.py
@@ -1,5 +1,7 @@
 
 def leap(year):
-  year = int(year)
-  if year % 4 == 0 and year % 100 != 0:
-    return str(year) + " is a leap year"
+    year = int(year)
+    if year % 4 == 0 and year % 100 != 0:
+        return str(year) + " is a leap year"
+    if year % 4 == 0 and year % 100 == 0 and year % 400 == 0:
+        return str(year) + " is a leap year"
