1b7ee4ab6db91667e4cf46870180d08594f134e5
megan black <blackme@oregonstate.edu>
2021-05-27 08:44:35 -0700

    Implemented Fizz, Buzz, and Fizzbuzz inclusions into the printing method to make the second test work

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,7 +2,12 @@
 def fb():
     lst = []
     for i in range(1,101):
-        lst.append(int(i))
+        if(i % 5 == 0 and i % 3 == 3):
+            lst.append("FizzBuzz")
+        elif(i % 3 == 0):
+            lst.append("Fizz")
+        elif(i % 5 == 0):
+            lst.append("Buzz")
+        else:
+            lst.append(str(i))
     return lst
-
-print(fb())
