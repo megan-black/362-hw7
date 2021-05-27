bda727050b654646d04cc925f66950316dc7bca6
megan black <blackme@oregonstate.edu>
2021-05-27 08:33:47 -0700

    Added third test, tesing years that are not a leap year

--- test_leapyear.py
+++ test_leapyear.py
@@ -2,10 +2,12 @@ import unittest
 import leapyear
 
 class TestCase(unittest.TestCase):
-  def test_good1(self):
-    self.assertEqual(leapyear.leap(2020), "2020 is a leap year")
-def test_good2(self):
-self.assertEqual(leapyear.leap(2000), "2000 is a leap year")
+    def test_good1(self):
+        self.assertEqual(leapyear.leap(2020), "2020 is a leap year")
+    def test_good2(self):
+        self.assertEqual(leapyear.leap(2000), "2000 is a leap year")
+    def test_good3(self):
+        self.assertEqual(leapyear.leap(1999), "1999 is not a leap year")
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
